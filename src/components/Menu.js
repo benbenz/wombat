715d542ea4fe4113273881f78e5de27312dfaaf5
Benoit Simon <benoit@sxnths.com>
2023-04-05 10:37:56 +0200

    - broaden click-ability of the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,15 +5,15 @@ import { useState } from 'react' ;
 import styles from '../styles/Menu.module.css';
 import Link from 'next/link';
 
-function MenuLink({menu}) {
+function MenuLink({children,menu}) {
     if(menu.link) {
         if(menu.link.endsWith(".html"))
-            return <Link href={"/page/html/"+menu.link}>{menu.title}</Link>
+            return <Link href={"/page/html/"+menu.link}>{children}</Link>
         else
-            return <Link href={menu.link}>{menu.title}</Link>
+            return <Link href={menu.link}>{children}</Link>
 
     } else
-        return menu.title
+        return <>{children}</>
 }
 
 export default function Menu(props) {
@@ -22,17 +22,19 @@ export default function Menu(props) {
         <ul className={styles.menuitems}>
         {props.menuItems.map((menu,index) => (
             <>
+            <MenuLink menu={menu}>
             <li key={index} className={styles.menuitem+` ${menu.spacing?"mt-9":"mt-2"}`}>
                 <span className={styles.menuitemicon}>
                 { menu.icon ? menu.icon : <RiDashboardFill/> }
                 </span>
 
-                <span className={styles.menuitemtext+` ${!props.open && "hidden"}`}><MenuLink menu={menu}/></span>
+                <span className={styles.menuitemtext+` ${!props.open && "hidden"}`}>{menu.title}</span>
                 {menu.submenu && props.open && (
                     <BsChevronDown className={`${submenuOpen && "rotate-180"}`} onClick={()=>setSubmenuOpen(!submenuOpen)}/>
                 )
                 }
             </li>
+            </MenuLink>
             {menu.submenu && submenuOpen && props.open && (
                 <ul>
                     {menu.submenuItems.map((submenuItem,index2) => (
