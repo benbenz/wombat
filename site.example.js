d7727caaa959976e39d61f830189213952be1e1d
Benoit Simon <benoit@sxnths.com>
2023-06-08 18:00:53 +0200

    - moved menu configuration to external /site.js file

---
+++ site.example.js
@@ -0,0 +1,37 @@
+import { BsFillImageFill, BsReverseLayoutTextSidebarReverse, BsPerson} from 'react-icons/bs' ;
+import { AiOutlineFileText, AiOutlineBarChart, AiOutlineMail, AiOutlineSetting, AiOutlineLogout } from 'react-icons/ai' ;
+
+const SiteConfig = {
+    
+  menuItems:
+  [
+    { title: "Intro" , link: "intro.html"} ,
+    { title: "Docs" , link: "/jsx/documents" , icon: <AiOutlineFileText/>} ,
+    { title: "Medias" , link: "/jsx/medias" , icon: <BsFillImageFill/> , spacing: true } ,
+    {
+      title: "Examples" ,
+      icon : <BsReverseLayoutTextSidebarReverse/>,
+      submenu: true ,
+      submenuItems : [
+        // this one will go through the mdxwrapper loader (it's supposed to wrap in a CSS reset - doesnt work right now because it comes FIRST !!!)
+        { title: "example1.mdx (thru)" , link: "/mdx/example1" } , 
+        // this one will go through the mdx proxy (same file)
+        { title: "example1.mdx (proxy)" , link: "example1.mdx" } , 
+        { title: "example2.mdx (thru)" , link: "/mdx/example2" } , 
+        { title: "example2.mdx (proxy)" , link: "example2.mdx" } , 
+        { title: "example3.md (thru)" , link: "/mdx/example3" } , 
+        { title: "notebooks.ipynb (thru)" , link: "/ipynb/notebooks" } ,
+        { title: "notebooks.ipynb (proxy)" , link: "notebooks.ipynb" } ,
+        { title: "ols.ipynb (thru)" , link: "/ipynb/ols" } 
+      ]
+    } ,
+    { title: "Analytics" , link: "/html/analytics" , icon: <AiOutlineBarChart/> },
+    // { title: "Inbox" , icon: <AiOutlineMail/> },
+    // { title: "Profile" , icon: <BsPerson/>, spacing: true },
+    // { title: "Settings" , icon: <AiOutlineSetting/> },
+    // { title: "Logout" , icon: <AiOutlineLogout/> },
+
+  ]  
+}
+
+export default SiteConfig ;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,4 @@
 import '@/styles/ipynbStyle.css'
-import { BsFillImageFill, BsReverseLayoutTextSidebarReverse, BsPerson} from 'react-icons/bs' ;
-import { AiOutlineFileText, AiOutlineBarChart, AiOutlineMail, AiOutlineSetting, AiOutlineLogout } from 'react-icons/ai' ;
 /*
 Order here is really important
 - some stuff override prism in global.css
@@ -11,39 +9,13 @@ Order here is really important
 import 'highlight.js/styles/atom-one-light.css';
 import '@/styles/globals.css'
 import Layout from '../components/LayoutDrawer.js'
+import SiteConfig from '../../site.js' ;
 
 export default function App({ Component, pageProps }) {
-  const menuItems = [
-    { title: "Intro" , link: "intro.html"} ,
-    { title: "Docs" , link: "/jsx/documents" , icon: <AiOutlineFileText/>} ,
-    { title: "Medias" , link: "/jsx/medias" , icon: <BsFillImageFill/> , spacing: true } ,
-    {
-      title: "Examples" ,
-      icon : <BsReverseLayoutTextSidebarReverse/>,
-      submenu: true ,
-      submenuItems : [
-        // this one will go through the mdxwrapper loader (it's supposed to wrap in a CSS reset - doesnt work right now because it comes FIRST !!!)
-        { title: "example1.mdx (thru)" , link: "/mdx/example1" } , 
-        // this one will go through the mdx proxy (same file)
-        { title: "example1.mdx (proxy)" , link: "example1.mdx" } , 
-        { title: "example2.mdx (thru)" , link: "/mdx/example2" } , 
-        { title: "example2.mdx (proxy)" , link: "example2.mdx" } , 
-        { title: "example3.md (thru)" , link: "/mdx/example3" } , 
-        { title: "notebooks.ipynb (thru)" , link: "/ipynb/notebooks" } ,
-        { title: "notebooks.ipynb (proxy)" , link: "notebooks.ipynb" } ,
-        { title: "ols.ipynb (thru)" , link: "/ipynb/ols" } 
-      ]
-    } ,
-    { title: "Analytics" , link: "/html/analytics" , icon: <AiOutlineBarChart/> },
-    // { title: "Inbox" , icon: <AiOutlineMail/> },
-    // { title: "Profile" , icon: <BsPerson/>, spacing: true },
-    // { title: "Settings" , icon: <AiOutlineSetting/> },
-    // { title: "Logout" , icon: <AiOutlineLogout/> },
-
-  ]  
+  
 
   const title = "Econ" ;
   const title_extra = "2023"
 
-  return <Layout menuItems={menuItems} title={title} title_extra={title_extra}><Component {...pageProps} /></Layout>
+  return <Layout menuItems={SiteConfig.menuItems} title={title} title_extra={title_extra}><Component {...pageProps} /></Layout>
 }
