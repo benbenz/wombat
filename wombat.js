87d655f70ba59d5a9f15a2c6347b470c356077ea
Benoit Simon <benoit@sxnths.com>
2023-11-15 15:19:12 +0100

    - now generating automatically site.json

--- wombat.js
+++ wombat.js
@@ -2,15 +2,20 @@ const path    = require('path');
 const fs      = require('fs');
 const process = require('process');
 
-function listFilesInDirectory(dir, fileType, fileList = [], parentPath = '') {
+function listFilesInDirectory(dir, fileList = [], parentPath = '') {
   const files = fs.readdirSync(dir);
   files.forEach(file => {
     const filePath = path.join(dir, file);
     const stat = fs.statSync(filePath);
+    let newdict = null ;
     if (stat.isDirectory()) {
-      listFilesInDirectory(filePath, fileType, fileList, path.join(parentPath, file));
-    } else if (!fileType || path.extname(file) === fileType) {
-      fileList.push(path.join(parentPath, file));
+      newdict = { title: file , submenu: true , submenuItems: [] } ;
+      listFilesInDirectory(filePath, newdict['submenuItems'], path.join(parentPath, file));
+      fileList.push(newdict) ;
+    } else {
+      newdict = { title: file , link: path.join(parentPath, file) } ;
+      //fileList.push(path.join(parentPath, file));
+      fileList.push(newdict)
     }
   });
 
@@ -27,6 +32,24 @@ function install(directory) {
     dest_site    = path.join('.','site.json') ;
     dest_indexjs = path.join('src','pages','index.js') ;
     dest_content = path.join('src','pages','content') ;
+
+    const siteFilePath = path.join(installdir,'site.json') ;
+    if(!fs.existsSync(siteFilePath)) {
+        const contentDir = path.join(installdir,'content') ;
+        siteJson = {
+            'title' : 'My Site' ,
+            'title_extra' : 'V1' ,
+            'menuItems' : [
+                {
+                    'title' : 'ALL FILES' ,
+                    'link' : '/list/_all'
+                }
+            ]
+        }
+        listFilesInDirectory(contentDir,siteJson['menuItems']) ;
+        fs.writeFileSync(siteFilePath,JSON.stringify(siteJson,null,4))
+    }
+
     fs.unlink(dest_site,(err)=>{});
     fs.unlink(dest_indexjs,(err)=>{});
     fs.unlink(dest_content,(err)=>{});
